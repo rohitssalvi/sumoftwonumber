7668159241a8a5027b3f031014c7c407a780ad45
rohitssalvi <rsalvi923@gmail.com>
2016-05-07 10:59:34 +0530

    Added files via upload

---
+++ src/sumoftwonumber/Twonumbersum.java
@@ -0,0 +1,15 @@
+package sumoftwonumber;
+
+public class Twonumbersum {
+	public static void main(String[] args) {
+		int firstdigit=5;
+		float seconddigit=4.0f;
+		
+		int sum;
+		
+		sum=(int) (firstdigit+seconddigit);
+		System.out.println(sum);
+		
+	}
+
+}
